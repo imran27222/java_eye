4c3876dd8c9103a4f47d343dc627fb0a48414099
alihaseeb <ali.haseeb@aigros.com>
2025-05-17 18:45:43 +0500

    fix: revamp

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const {
   connectRedis
 } = require("./src/utilities/redis");
 
-await connectRedis();
+connectRedis();
 
 const app = express();
 app.set("views", path.join(__dirname, "src/templates"));
